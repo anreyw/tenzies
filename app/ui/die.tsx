00005de8438754c4023bcb199e7c0925c8f0f088
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-02-28 15:52:34 -0800

    Fix: Added DieProps interface to remote typescript 'any' error

--- app/ui/die.tsx
+++ app/ui/die.tsx
@@ -1,4 +1,8 @@
-export default function Die(props) {
+interface DieProps {
+  value: number;
+}
+
+export default function Die(props: DieProps) {
   return (
     <button className="size-12 bg-white rounded-md shadow-md text-2xl font-bold">{props.value}</button>    
   )
