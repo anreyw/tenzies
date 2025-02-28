23a2296833d045b2fa535f69aeb44e5add85c5fb
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-02-28 07:52:01 -0800

    Refactored Dice component into separate module.
    - Turned off ssr for dice client component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,14 @@
 'use client'
 
-import Die from '@/app/ui/die'
-import { useState, useEffect } from 'react'
+import dynamic from 'next/dynamic'
 
-// useEffect is needed to prevent a client-server mismatch that is
-// caused by random number generator running before *and* after
-// hydration.  I suspect that there is a better way of doing this that
-// probably involves using the dynamic componenp import and no ssr.
+const Dice = dynamic(() => import('@/app/ui/dice'), { ssr: false })
 
 export default function Home() {
-  const [diceNums, setDiceNums] = useState([1,1,1,1,1,1,1,1,1,1,])
-  const [isClient, setIsClient] = useState(false)
-
-  useEffect(() => {
-    // This only runs on the client after hydration
-    setDiceNums(generateNums())
-    setIsClient(true)
-  }, [])
-
-  function generateNums() {
-    return Array.from({length: 10}, item => Math.ceil(Math.random() * 5))
-  }
-
-  const dice = diceNums.map(num => (
-    <Die value={num} />
-  ))
-
-  function handleClick() {
-    setDiceNums(generateNums())
-  }
-  
   return (
     <main className="bg-zinc-100 size-4/5 rounded-lg flex flex-col items-center justify-center gap-8">
       <h1 className="text-3xl font-bold">Tenzies</h1>
-      <div className="grid grid-cols-2 md:grid-cols-5 gap-4">
-	{dice}
-      </div>
-      <button onClick={handleClick} className="bg-indigo-600 px-6 py-2 rounded-lg text-white text-2xl">Roll</button>
+      <Dice />
     </main>
   )
 }
---
+++ app/ui/dice.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import Die from '@/app/ui/die'
+import { useState } from 'react'
+
+export default function Dice() {
+  const [diceNums, setDiceNums] = useState(generateNums())
+
+  function generateNums() {
+    return Array.from({length: 10},
+      () => ({
+	value: Math.ceil(Math.random() * 5),
+	isHeld: false })
+    )
+  }
+
+  const dice = diceNums.map(die => (
+    <Die value={die.value} />
+  ))
+
+  function handleClick() {
+    setDiceNums(generateNums())
+  }
+  
+  return (
+    <>
+      <div className="grid grid-cols-2 md:grid-cols-5 gap-4">
+	{dice}
+      </div>
+      <button onClick={handleClick} className="bg-indigo-600 px-6 py-2 rounded-lg text-white text-2xl">Roll</button>
+    </>
+  )
+}
+  
