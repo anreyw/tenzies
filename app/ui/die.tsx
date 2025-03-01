e533371c93758bf2db336f68d461916cd87782e8
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-02-28 16:30:07 -0800

    Feature: Added pointer cursor tailwind class to the Die button.

--- app/ui/die.tsx
+++ app/ui/die.tsx
@@ -4,6 +4,6 @@ interface DieProps {
 
 export default function Die(props: DieProps) {
   return (
-    <button className="size-12 bg-white rounded-md shadow-md text-2xl font-bold">{props.value}</button>    
+    <button className="cursor-pointer size-12 bg-white rounded-md shadow-md text-2xl font-bold">{props.value}</button>    
   )
 }
