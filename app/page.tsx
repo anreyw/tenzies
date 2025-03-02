bc406a2cf2525b6d6ca11d8d368cf1668bea0b39
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-03-01 16:02:34 -0800

    Fix: improved CSS styling to fit contents better.

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,9 @@ const Dice = dynamic(() => import('@/app/ui/dice'), { ssr: false })
 
 export default function Home() {
   return (
-    <main className="bg-zinc-100 size-4/5 rounded-lg flex flex-col items-center justify-center gap-8">
+    <main className="bg-zinc-100 p-12 rounded-lg flex flex-col items-center justify-center gap-8 max-w-4/5">
       <h1 className="text-3xl font-bold">Tenzies</h1>
+      <p className="text-xl max-w-md text-center">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <Dice />
     </main>
   )
